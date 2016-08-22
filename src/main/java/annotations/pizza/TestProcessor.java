c9b43b0377d12a3e35b497ead126a0c1e83c5c4e
nohorbee <nohorbee@gmail.com>
2016-08-21 21:30:12 -0300

    codility

--- src/main/java/annotations/pizza/TestProcessor.java
+++ src/main/java/annotations/pizza/TestProcessor.java
@@ -8,7 +8,7 @@ public class TestProcessor {
         String arguments[] = {"-proc:only", "-processor", "annotations.pizza.FactoryProcessor", "src/main/java/annotations/pizza/MargaritaPizza.java"};
         try {
             System.out.println("Start processing");
-            com.sun.tools.javac.Main.main(arguments);
+            //com.sun.tools.javac.Main.main(arguments);
         } catch (Exception e) {
             e.printStackTrace();
         }
